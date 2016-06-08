8d416f15e0431195549a895e6ae6257ff0bcd4e9
Ian <ianbenedict@gmail.com>
2016-06-08 18:56:47 -0400

    :star: :sparkles: Use coveralls in travis script and clean up.

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
-var Discord = require('discord.js')
+require('./globals')
 
-var bot = new Discord.Client()
+var metabot = require('./metabot')
 
-bot.on('message', (message) => {
-  if (message.content === 'ping') {
-    bot.reply(message, 'pong')
-  }
-})
-
-bot.loginWithToken(process.env.METABOT_TOKEN)
+// Log into Discord with bot token
+metabot.connect({ token: process.env.METABOT_TOKEN })
