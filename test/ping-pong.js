880e2b3619a96549caea5dbc64e8759c5636a905
John Tai <zapyre_1@hotmail.com>
2016-06-08 12:20:10 -0400

    Adding in ping-pong test and check-in test

---
+++ test/ping-pong.js
@@ -0,0 +1,11 @@
+var Discord = require("discord.js");
+
+var mybot = new Discord.Client();
+
+mybot.on("message", function(message) {
+    if(message.content === "ping") {
+        mybot.reply(message, "pong");
+    }
+});
+
+mybot.loginWithToken("token");
