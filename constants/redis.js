874df59da9cc6004e22108612f119f02149717f1
Ian Benedict <ianbenedict@gmail.com>
2016-07-07 08:01:08 -0400

    :star: Add missing constants file.

---
+++ constants/redis.js
@@ -0,0 +1,20 @@
+/**
+ * Delimiter used to separate the namespace and key.
+ * @type {String}
+ */
+const NAMESPACE_DELIMITER = ':'
+
+
+
+/**
+ * Key for the set used to store namespaces.
+ * @type {String}
+ */
+const NAMESPACE_KEY = 'namespaces'
+
+
+
+module.exports = {
+  NAMESPACE_DELIMITER,
+  NAMESPACE_KEY
+}
