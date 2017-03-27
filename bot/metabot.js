484d2ee8e87096efd26316674a0bc5f164f10e89
Ian Benedict <ianbenedict@gmail.com>
2017-03-27 04:11:44 -0400

    :sparkles:

--- bot/metabot.js
+++ bot/metabot.js
@@ -1,14 +1,10 @@
-let Discordie   = require('discordie')
+let Discordie = require('discordie')
 
 let metabotUtil = requireRoot('bot/utils/metabot')
 
-let Events      = Discordie.Events
-let client      = new Discordie()
-
-
+let Events = Discordie.Events
+let client = new Discordie()
 
 client.Dispatcher.on(Events.MESSAGE_CREATE, metabotUtil.handleMessageCreate)
-
-
 
 module.exports = client
